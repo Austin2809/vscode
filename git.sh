93e58b51f53808ffe57fff7be1dc549faf540660
Austin2809 <133880171+Austin2809@users.noreply.github.com>
2023-07-12 18:46:30 +0000

    Commit message explaining your changes

---
+++ git.sh
@@ -0,0 +1,8 @@
+git clone git@github.com:YourName/BluescreenSimulator.git
+git clone https://github.com/YourName/BluescreenSimulator.git
+
+git checkout -b  ANameDescribingYourBranch
+
+git add .
+git commit -m "Commit message explaining your changes"
+git push
